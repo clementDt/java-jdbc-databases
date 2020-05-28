be0a39913d3a0f620f55f40d9575f0825b5cb1d8
Clément DUMONT <cdumont1506@gmail.com>
2020-05-28 16:02:29 +0200

    Module 3

--- src/main/java/com/pluralsight/order/dao/GetOrderDao.java
+++ src/main/java/com/pluralsight/order/dao/GetOrderDao.java
@@ -19,6 +19,7 @@ public class GetOrderDao {
 
     /**
      * Constructor
+     *
      * @param database Database object
      */
     public GetOrderDao(Database database) {
@@ -27,16 +28,24 @@ public class GetOrderDao {
 
     /**
      * Gets an order by its ID
+     *
      * @param paramsDto Object with the parameters for the operation
      * @return Object with the main information of an order
      */
     public OrderDto getOrderById(ParamsDto paramsDto) {
         OrderDto orderDto = null;
 
-        try (Connection con = null;
+        try (Connection con = database.getConnection();
              PreparedStatement ps = createPreparedStatement(con, paramsDto.getOrderId());
              ResultSet rs = createResultSet(ps)
         ) {
+            while (rs.next()) {
+                orderDto = new OrderDto();
+                orderDto.setCustomerId(rs.getLong("order_customer_id"));
+                orderDto.setOrderId(rs.getLong("order_id"));
+                orderDto.setDate(rs.getDate("order_date"));
+                orderDto.setStatus(rs.getString("order_status"));
+            }
 
         } catch (SQLException ex) {
             ExceptionHandler.handleException(ex);
@@ -47,23 +56,26 @@ public class GetOrderDao {
 
     /**
      * Creates a PreparedStatement object to get an order
-     * @param con Connnection object
+     *
+     * @param con     Connnection object
      * @param orderId Order ID to set on the PreparedStatement
      * @return A PreparedStatement object
      * @throws SQLException In case of an error
      */
     private PreparedStatement createPreparedStatement(Connection con, long orderId) throws SQLException {
-
-        return null;
+        PreparedStatement preparedStatement = con.prepareStatement(query);
+        preparedStatement.setLong(1, orderId);
+        return preparedStatement;
     }
 
     /**
      * Creates a ResultSet object to get the results of the query
+     *
      * @param ps PreparedStatement object to create the query
      * @return A ResultSet object
      * @throws SQLException In case of an error
      */
     private ResultSet createResultSet(PreparedStatement ps) throws SQLException {
-        return null;
+        return ps.executeQuery();
     }
 }
--- src/main/java/com/pluralsight/order/util/ExceptionHandler.java
+++ src/main/java/com/pluralsight/order/util/ExceptionHandler.java
@@ -9,9 +9,14 @@ public class ExceptionHandler {
 
     /**
      * Method to extract and print information from a SQLException
+     *
      * @param sqlException Exception from which information will be extracted
      */
     public static void handleException(SQLException sqlException) {
 
+        System.out.println(sqlException.getErrorCode() + " "
+                + sqlException.getSQLState() + " "
+                + sqlException.getMessage());
+        sqlException.printStackTrace();
     }
 }
